549841348ba089cda0b4d6e96af513f003ca0272
dankvis <talexanderd99@gmail.com>
2021-12-14 05:29:51 -0600

    Visualizer works! Exporter isn't necessary because the graphviz python library handles the dot conversion, which was easily done in the visualizer

--- src/PGraph.py
+++ src/PGraph.py
@@ -1,3 +1,5 @@
+from PNode import DirNode, FileNode
+
 class PGraph:
     def __init__(self, root, all_nodes):
         self.root = root
@@ -5,25 +7,19 @@ class PGraph:
         self.active_nodes = all_nodes
 
     def method_mode(self):
-        #return a PGraph with method node dependencies
         pass
 
     def class_mode(self):
-        #return a PGraph with class node dependencies
         pass
 
     def file_mode(self):
-        #return a PGraph with file node dependencies
-        pass
+        self.active_nodes = {x.name:x for x in self.all_nodes.values() if isinstance(x, FileNode)}
 
     def dir_mode(self):
-        #return a PGraph with dir node dependencies
-        pass
+        self.active_nodes = {x.name:x for x in self.all_nodes.values() if isinstance(x, DirNode)}
 
     def up(self):
-        #return a PGraph with the next highest level of nodes
         pass
 
     def down(self):
-        #return a PGraph with the next lowest level of nodes
         pass
--- src/PGraphVisualizer.py
+++ src/PGraphVisualizer.py
@@ -1,4 +1,7 @@
 import graphviz
+from PGraph import PGraph
+from PNode import DirNode, FileNode
+from PGraphBuilder import PGraphBuilder
 
 class PGraphVisualizer:
     def __init__(self, pgraph):
@@ -8,6 +11,22 @@ class PGraphVisualizer:
         graph = graphviz.Digraph(format='png')
         for node in self.pgraph.active_nodes.values():
             graph.node(node.name, node.name)
-            for dest in node.defines:
+            for dest in node.calls.values():
                 graph.edge(node.name, dest.name)
         graph.render('graph.gv', view=True)
+
+if __name__ == '__main__':
+    root = DirNode('root')
+    a = FileNode('a')
+    b = FileNode('b')
+    builder = PGraphBuilder()
+    builder.add_node(root)
+    builder.add_node(a)
+    builder.add_node(b)
+    builder.add_def(a, root)
+    builder.add_def(b, root)
+    builder.add_call(a,b)
+    pgraph = builder.build_pgraph()
+    pgraph.dir_mode()
+    viz = PGraphVisualizer(pgraph)
+    viz.visualize()
