9bd0c90d83de97b62872446dd544e3c821f4e81d
Alexander Davis <talexanderd99@gmail.com>
2021-12-14 23:50:44 -0600

    added check to ensure that only active nodes are considered for calls

--- src/PGraphVisualizer.py
+++ src/PGraphVisualizer.py
@@ -10,7 +10,7 @@ class PGraphVisualizer:
         for node in self.pgraph.active_nodes.values():
             node_short_name = get_file_name_at_end_of_path(node.name)
             graph.node(node_short_name, node_short_name)
-            for dest in node.calls.values():
+            for dest in [x for x in node.calls.values() if x in self.pgraph.active_nodes.values()]:
                 dest_short_name = get_file_name_at_end_of_path(dest.name)
                 graph.edge(dest_short_name, node_short_name)
         graph.render('graph.gv', view=True)
